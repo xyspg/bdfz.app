e45c95aa8f87b6769d86d064a1ec814f72d817ec
Kang Jiaming <kangjm2005@hotmail.com>
2023-05-15 17:45:44 +0800

    feat: GitHub Oauth

--- pages/login.tsx
+++ pages/login.tsx
@@ -10,7 +10,7 @@ import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import Image from 'next/image'
+import { GitHubLogoIcon} from '@radix-ui/react-icons'
 
 const LoginPage = () => {
   const router = useRouter()
@@ -236,6 +236,36 @@ const LoginPage = () => {
         scopes: 'email',
       },
     })
+    if (error) {
+      toast.error(`${error.message}`, {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      })
+    }
+  }
+
+  async function signInWithGitHub() {
+    const { data, error } = await supabaseClient.auth.signInWithOAuth({
+      provider: 'github',
+    })
+    if (error) {
+      toast.error(`${error.message}`, {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      })
+    }
   }
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -355,17 +385,34 @@ const LoginPage = () => {
                   : '注册'}
               </Button>
               {loginOrSignup !== 'reset' && (
-              <Button className="w-full shadow-md bg-white hover:bg-slate-100 text-neutral-700 flex flex-row justify-center gap-2" onClick={signInWithAzure}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23" width={20} height={20}>
-                  <path fill="#f3f3f3" d="M0 0h23v23H0z"/>
-                  <path fill="#f35325" d="M1 1h10v10H1z"/>
-                  <path fill="#81bc06" d="M12 1h10v10H12z"/>
-                  <path fill="#05a6f0" d="M1 12h10v10H1z"/>
-                  <path fill="#ffba08" d="M12 12h10v10H12z"/>
-                </svg>
-                <p> 使用 Microsoft 登录</p>
-              </Button>
-                )}
+                <div className='flex flex-col gap-4'>
+                  <Button
+                    className="w-full shadow-md bg-white hover:bg-slate-100 text-neutral-700 flex flex-row justify-center gap-2"
+                    onClick={signInWithAzure}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 23 23"
+                      width={20}
+                      height={20}
+                    >
+                      <path fill="#f3f3f3" d="M0 0h23v23H0z" />
+                      <path fill="#f35325" d="M1 1h10v10H1z" />
+                      <path fill="#81bc06" d="M12 1h10v10H12z" />
+                      <path fill="#05a6f0" d="M1 12h10v10H1z" />
+                      <path fill="#ffba08" d="M12 12h10v10H12z" />
+                    </svg>
+                    <p> 使用 Microsoft 登录</p>
+                  </Button>
+                  <Button
+                    className="w-full shadow-md bg-white hover:bg-slate-100 text-neutral-700 flex flex-row justify-center gap-2"
+                    onClick={signInWithGitHub}
+                  >
+                   <GitHubLogoIcon width='20' height='20' />
+                    <p> 使用 GitHub 登录</p>
+                  </Button>
+                </div>
+              )}
               <div className="flex flex-row justify-around">
                 {loginOrSignup !== 'reset' && (
                   <p
