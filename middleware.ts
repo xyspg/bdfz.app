b9d8d6474d2c53a2155ca7fabe7ae9bf4e71638c
Kang Jiaming <kangjm2005@hotmail.com>
2023-05-16 20:29:42 +0800

    middleware

--- middleware.ts
+++ middleware.ts
@@ -7,25 +7,25 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   // Create authenticated Supabase Client.
   const supabase = createMiddlewareSupabaseClient({ req, res })
-  await supabase.auth.refreshSession()
+  let cookie = req.cookies.get('supabase-auth-token')?.value
 
   // Check if we have a session
   const {
     data: { session },
   } = await supabase.auth.getSession()
-
-  // Check auth condition
-  if (session) {
-    // Authentication successful, forward request to protected route.
+  if (req.nextUrl.pathname.startsWith('/') && cookie) {
     return res
+  } else if (session) {
+    return res
+  } else {
+    const redirectUrl = req.nextUrl.clone()
+    redirectUrl.pathname = '/login'
+    redirectUrl.searchParams.set(`redirect`, req.nextUrl.pathname)
+    return NextResponse.redirect(redirectUrl)
   }
 
-  // Auth condition not met, redirect to home page.
-  const redirectUrl = req.nextUrl.clone()
-  redirectUrl.pathname = '/login'
-  redirectUrl.searchParams.set(`redirect`, req.nextUrl.pathname)
-  return NextResponse.redirect(redirectUrl)
 }
+
 export const config = {
-  matcher: ['/api','/profile','/password','/'],
-};
+  matcher: ['/api', '/profile', '/password', '/'],
+}
