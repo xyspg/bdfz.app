0bbd7e1d3bdc8293461c359ff269de2787375b87
Kang Jiaming <kangjm2005@hotmail.com>
2023-05-16 20:34:09 +0800

    cookie

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
+
 export async function middleware(req: NextRequest) {
   // We need to create a response and hand it to the supabase client to be able to modify the response headers.
   const res = NextResponse.next()
