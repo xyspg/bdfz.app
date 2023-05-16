c322603f718aadb7ab45e99c4846d16307d575fa
Kang Jiaming <kangjm2005@hotmail.com>
2023-05-16 20:37:04 +0800

    fix

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,11 @@ import { NextResponse } from 'next/server'
 
 
 export async function middleware(req: NextRequest) {
-  // We need to create a response and hand it to the supabase client to be able to modify the response headers.
   const res = NextResponse.next()
-  // Create authenticated Supabase Client.
   const supabase = createMiddlewareSupabaseClient({ req, res })
   let cookie = req.cookies.get('supabase-auth-token')?.value
 
-  // Check if we have a session
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
-  if (req.nextUrl.pathname.startsWith('/') && cookie) {
-    return res
-  } else if (session) {
+  if (cookie) {
     return res
   } else {
     const redirectUrl = req.nextUrl.clone()
@@ -24,7 +16,6 @@ export async function middleware(req: NextRequest) {
     redirectUrl.searchParams.set(`redirect`, req.nextUrl.pathname)
     return NextResponse.redirect(redirectUrl)
   }
-
 }
 
 export const config = {
