14f1fb96ce440e0e9b0bf7eac1dd6ba880a86682
Kang Jiaming <kangjm2005@hotmail.com>
2023-07-06 09:30:15 +0800

    fix error

--- components/ChatDialog.tsx
+++ components/ChatDialog.tsx
@@ -21,7 +21,6 @@ import FingerprintJS from '@fingerprintjs/fingerprintjs'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
-import { Components } from 'react-markdown'
 import ChatPanel from "@/components/ChatPanel";
 import FeedbackPanel from '@/components/chat/FeedbackPanel'
 import SampleQuestion from '@/components/chat/SampleQuestion'
@@ -426,7 +425,7 @@ export const ChatDialog: React.FC<ChatHistoryProps> = ({ History, Mode }) => {
     if (lastMessage.role === 'user') {
       handleConfirm(lastMessage.content)
     }
-
+  }
   const handleOnQuery = (query: string) => {
     handleConfirm(query)
   }
