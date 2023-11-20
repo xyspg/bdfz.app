a65c6c43c2417d028ff14e0c3119f9a86a5b7bb7
Kang Jiaming <kangjm2005@hotmail.com>
2023-11-20 17:08:58 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ['en-US', 'zh-CN','zh-TW', 'ja','ru','ko','fr'],
+    locales: ['en-US', 'zh-CN', 'ja'],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'zh-CN',
